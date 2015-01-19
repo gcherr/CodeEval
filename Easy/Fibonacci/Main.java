5c4799afe572ec1afc741ae32aea9827e28ce7e0
gcherrer <gcherrer@ucsc.edu>
2015-01-18 21:32:44 -0800

    Fibonacci finished

---
+++ Easy/Fibonacci/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+import static java.lang.System.*;
+import java.lang.Integer;
+
+public class Main{
+   static int fib(int base){
+      if (base == 0)
+         return 0;
+      else if (base == 1)
+         return 1;
+      else
+         return (fib(base-1) + fib(base-2)); 
+   }
+   public static void main(String[] args){
+      try {
+         Scanner infile = new Scanner (new File(args[0]));
+         while(infile.hasNextLine()){
+            String line = infile.nextLine();
+            out.printf("%d\n",fib(Integer.parseInt(line)));
+         }
+         infile.close();
+         exit(0);
+      } catch (IOException error){
+         err.printf("%s%n", error.getMessage());
+         exit(1);
+      }
+   }
+}
